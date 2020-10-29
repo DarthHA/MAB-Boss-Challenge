5cd82a47f2a120aebfb3a2ab4b862caa4d74df99
DarthHA <73414822+DarthHA@users.noreply.github.com>
2020-10-29 23:13:58 +0800

    Add files via upload

---
+++ Projectiles/MeteorPlayerNPC/MeteorTransform.cs
@@ -0,0 +1,53 @@
+﻿using MABBossChallenge.NPCs;
+using MABBossChallenge.NPCs.MeteorPlayerNPC;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace MABBossChallenge.Projectiles.MeteorPlayerNPC
+{
+    public class MeteorTransform : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("");
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = 1;
+            projectile.height = 1;
+            projectile.scale = 1.0f;
+            projectile.friendly = false;
+            projectile.hostile = false;
+            projectile.timeLeft = 1;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.damage = 0;
+            projectile.penetrate = -1;
+            projectile.netImportant = true;
+            projectile.hide = true;
+        }
+        public override void AI()
+        {
+            projectile.velocity = Vector2.Zero;
+
+        }
+        public override void Kill(int timeLeft)
+        {
+            if (NPC.AnyNPCs(ModContent.NPCType<MeteorPlayerNPC1>()) && !NPC.AnyNPCs(ModContent.NPCType<MeteorPlayerNPC2>()))
+            {
+                NPC npctmp = (NPC)(Main.npc[NPC.FindFirstNPC(ModContent.NPCType<MeteorPlayerNPC1>())].Clone());
+
+                Main.npc[NPC.FindFirstNPC(ModContent.NPCType<MeteorPlayerNPC1>())].Transform(ModContent.NPCType<MeteorPlayerNPC2>());
+                Main.npc[NPC.FindFirstNPC(ModContent.NPCType<MeteorPlayerNPC2>())].homeless = npctmp.homeless;
+                Main.npc[NPC.FindFirstNPC(ModContent.NPCType<MeteorPlayerNPC2>())].homeTileX = npctmp.homeTileX;
+                Main.npc[NPC.FindFirstNPC(ModContent.NPCType<MeteorPlayerNPC2>())].homeTileY = npctmp.homeTileY;
+                Main.npc[NPC.FindFirstNPC(ModContent.NPCType<MeteorPlayerNPC2>())].oldHomeless = npctmp.oldHomeless;
+                Main.npc[NPC.FindFirstNPC(ModContent.NPCType<MeteorPlayerNPC2>())].oldHomeTileX = npctmp.oldHomeTileX;
+                Main.npc[NPC.FindFirstNPC(ModContent.NPCType<MeteorPlayerNPC2>())].oldHomeTileY = npctmp.oldHomeTileY;
+
+            }
+        }
+
+    }
+}
---
+++ Projectiles/MeteorPlayerNPC/PhaseCounter.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace MABBossChallenge.Projectiles.MeteorPlayerNPC
+{
+    public class PhaseCounter : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("PhaseCounter");
+            DisplayName.AddTranslation(GameCulture.Chinese, "碰撞判断");
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = 60;
+            projectile.height = 60;
+            projectile.scale = 1.0f;
+            projectile.friendly = true;
+            projectile.hostile = false;
+            projectile.timeLeft = 999999;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+            projectile.damage = 100;
+            projectile.penetrate = -1;
+            projectile.netImportant = true;
+            projectile.hide = true;
+            projectile.idStaticNPCHitCooldown = 0;
+            projectile.usesIDStaticNPCImmunity = true;
+        }
+        public override void AI()
+        {
+            if (!Main.npc[(int)projectile.ai[0]].active)
+            {
+                projectile.active = false;
+                return;
+            }
+
+            NPC owner = Main.npc[(int)projectile.ai[0]];
+            projectile.Center = owner.Center;
+            projectile.damage = (int)(owner.damage * 0.4f);
+            projectile.penetrate = -1;
+            if (!owner.dontTakeDamageFromHostiles)
+            {
+                projectile.active = false;
+            }
+        }
+        public override bool PreKill(int timeLeft)
+        {
+            NPC owner = Main.npc[(int)projectile.ai[0]];
+            if (owner.dontTakeDamageFromHostiles)
+            {
+                Projectile.NewProjectile(projectile.Center, Vector2.Zero, projectile.type, projectile.damage, projectile.knockBack, projectile.owner, projectile.ai[0], projectile.ai[1]);
+            }
+            return base.PreKill(timeLeft);
+        }
+        public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+        {
+            damage += target.defense / 2;
+        }
+
+    }
+}
