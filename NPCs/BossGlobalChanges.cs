06036053a92468b2b08d02415a53a48b21b63497
DarthHA <73414822+DarthHA@users.noreply.github.com>
2020-11-09 09:06:47 +0800

    Add files via upload

--- NPCs/BossGlobalChanges.cs
+++ NPCs/BossGlobalChanges.cs
@@ -1,9 +1,7 @@
+using MABBossChallenge.Buffs;
 using MABBossChallenge.NPCs.PlayerBoss;
 using MABBossChallenge.Utils;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -14,8 +12,90 @@ namespace MABBossChallenge.NPCs
     {
 
         public override bool InstancePerEntity => true;
-
-
+        public int CurrentHealth;    //寄生血量
+        public override void SetDefaults(NPC npc)
+        {
+            if (npc.type == NPCID.AncientLight && NPC.AnyNPCs(ModContent.NPCType<StardustSummonerBoss>()))
+            {
+                npc.dontTakeDamage = true;
+                for (int i = 0; i < npc.buffImmune.Length; i++)
+                {
+                    npc.buffImmune[i] = true;
+                }
+            }
+            CurrentHealth = npc.lifeMax;
+        }
+        public override void UpdateLifeRegen(NPC npc, ref int damage)
+        {
+            if (npc.HasBuff(ModContent.BuffType<LifeFlare>()))
+            {
+                if (npc.lifeRegen > 0)
+                {
+                    npc.lifeRegen = 0;
+                }
+                npc.lifeRegen -= 240;
+            }
+        }
+        public override void ModifyHitByItem(NPC npc, Player player, Item item, ref int damage, ref float knockback, ref bool crit)
+        {
+            DebuffDamage(npc, ref damage);
+        }
+        public override void ModifyHitByProjectile(NPC npc, Projectile projectile, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+        {
+            DebuffDamage(npc, ref damage);
+        }
+        private void DebuffDamage(NPC npc,ref int damage)
+        {
+            if (npc.HasBuff(ModContent.BuffType<ManaFlare>()))
+            {
+                damage = (int)(damage * 1.25f);
+            }
+            if (npc.HasBuff(ModContent.BuffType<JusticeJudegmentBuff>()))
+            {
+                if (damage - npc.defense / 2 < npc.lifeMax / 10)
+                {
+                    damage += npc.defense / 2;
+                }
+                damage += npc.lifeMax / 10;
+            }
+        }
+        public override void TownNPCAttackStrength(NPC npc, ref int damage, ref float knockback)
+        {
+            DebuffDamage2(npc, ref damage);
+        }
+        public override void ModifyHitNPC(NPC npc, NPC target, ref int damage, ref float knockback, ref bool crit)
+        {
+            DebuffDamage2(npc, ref damage);
+        }
+        public override void ModifyHitPlayer(NPC npc, Player target, ref int damage, ref bool crit)
+        {
+            DebuffDamage2(npc, ref damage);
+            if (npc.type == NPCID.AncientLight && NPC.AnyNPCs(ModContent.NPCType<StardustSummonerBoss>()))
+            {
+                damage /= 2;
+            }
+        }
+        private void DebuffDamage2(NPC npc, ref int damage)
+        {
+            if (npc.HasBuff(ModContent.BuffType<DamageFlare>()))
+            {
+                damage = (int)(damage * 0.66f);
+            }
+        }
+        public override void PostAI(NPC npc)
+        {
+            if (npc.HasBuff(ModContent.BuffType<ImprovedCelledBuff>()))
+            {
+                if (npc.life <= CurrentHealth)
+                {
+                    CurrentHealth = npc.life;
+                }
+                else
+                {
+                    npc.life = CurrentHealth;
+                }
+            }
+        }
         public override bool PreAI(NPC npc)
         {
 
@@ -24,7 +104,6 @@ namespace MABBossChallenge.NPCs
                 case NPCID.AncientLight:
                     if (NPC.AnyNPCs(ModContent.NPCType<StardustSummonerBoss>()))
                     {
-                        npc.dontTakeDamage = true;
                         npc.position += npc.velocity * 0.75f;
                     }
                     break;
